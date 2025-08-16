43443996d61d75432e0b6fd2f56157a76fd433c3
manish <manishshrivastav423@gmail.com>
2025-08-16 23:56:28 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -37,11 +37,10 @@ console.log(`✅ MongoDB connected`);
 // }
 
 // ✅ Instead of app.listen, just export to make it live on vercel
-// const PORT=process.env.PORT||8000;
+const PORT=process.env.PORT||8000;
 
-// app.listen(PORT,()=>{
-//   console.log(`server running on port ${PORT}`)
-// })}
-
+app.listen(PORT,()=>{
+  console.log(`server running on port ${PORT}`)
+})
 
 export default app;

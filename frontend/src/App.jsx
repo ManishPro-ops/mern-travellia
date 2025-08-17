047d9958411dc6d26867b582b89d284d34616afe
manish <manishshrivastav423@gmail.com>
2025-08-17 07:51:22 +0530

    renamed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Pricing from "./pages/Pricing";
-import Services from "./pages/services";
+import Services from "./pages/Services";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Hoteldetail from "./pages/Hoteldetail";

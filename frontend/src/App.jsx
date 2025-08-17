f03c01ec57b48bf7ce56878a36ddb16d346b8f18
manish <manishshrivastav423@gmail.com>
2025-08-17 07:49:33 +0530

    renamed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Pricing from "./pages/pricing";
+import Pricing from "./pages/Pricing";
 import Services from "./pages/services";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
